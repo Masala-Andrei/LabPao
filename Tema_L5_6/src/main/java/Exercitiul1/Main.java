4630e8092cc2676466d01ae68d173cf194d8c385
unknown <andrei.masala@yahoo.com>
2025-04-22 01:10:30 +0300

    Tema Lab5_6

---
+++ Tema_L5_6/src/main/java/Exercitiul1/Main.java
@@ -0,0 +1,25 @@
+package Exercitiul1;
+
+public class Main {
+    public static void main(String[] args) {
+        new OutTask("Mesaj").execute();
+        new CounterOutTask().execute();
+        new OutTask("Luni 29 aprilie sedinta in departamentul IT").execute();
+        new CounterOutTask().execute();
+        new CounterOutTask().execute();
+        new RandomOutTask().execute();
+        new RandomOutTask().execute();
+        new CounterOutTask().execute();
+        new RandomOutTask().execute();
+        new CounterOutTask().execute();
+        new RandomOutTask().execute();
+        new RandomOutTask().execute();
+        new RandomOutTask().execute();
+        new RandomOutTask().execute();
+        new RandomOutTask().execute();
+        new RandomOutTask().execute();
+        new RandomOutTask().execute();
+        new RandomOutTask().execute();
+        new RandomOutTask().execute();
+    }
+}
